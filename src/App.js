17e9ed65fccfd78b4cd2aa635b84e0a2ba9bace6
Paloma Araujo <palomaraujo4@gmail.com>
2021-04-11 20:00:12 -0300

    Criação dos componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
 import Navigation from './components/navigation/Navigation'
+import Logo from './components/logo/Logo'
+import Rank from './components/rank/Rank'
+import ImageLinkForm from './components/imageLinkForm/ImageLinkForm.js'
+import Particles from 'react-particles-js'
+
+const particlesOptions = {
+  particles: {
+      number: {
+        value:60,
+        density: {
+          enable: true,
+          value_area:800
+        }
+      }
+  }
+}
 
 function App() {
   return (
     <div className="App">
+      <Particles className='particles'
+                params={particlesOptions} />
       <Navigation />
-      {/*<Logo />
+      <Logo />
+      <Rank />
       <ImageLinkForm />
-      <FaceRecognition /> */}
+      {/*<FaceRecognition /> */}
     </div>
   );
 }
