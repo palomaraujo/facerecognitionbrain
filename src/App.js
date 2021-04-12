294504ae17a8a5dd868f141b39f86888a16499d6
Paloma Araujo <palomaraujo4@gmail.com>
2021-04-12 19:54:27 -0300

    Implementação do box de reconhecimento facial

--- src/App.js
+++ src/App.js
@@ -34,21 +34,35 @@ class  App extends Component {
   }
 
   calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+
+    }
 
   }
+
+  displayFaceBox = (box) => {
+    
+    this.setState({ box: box})
+    console.log(this.state.box)
+  }
+
   onInputChange = (event) =>{
     this.setState({ input: event.target.value }) 
   }
 
   onButtonSubmit = () => {
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-      function (response){
-        this.calculateFaceLocation(response);
-      },
-      function(err){
-        //erro
-      }
-    );
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err));
+    
   }
 
   render(){
@@ -60,7 +74,7 @@ class  App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm  onInputChange = {this.onInputChange} onButtonSubmit= {this.onButtonSubmit}/>
-        <FaceRecognition imageUrl = { this.state.input }/>
+        <FaceRecognition box = {this.state.box} imageUrl = { this.state.input }/>
       </div>
     );
   }
